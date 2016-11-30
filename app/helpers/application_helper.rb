a3fd329d71d74dac9e418ca9582a94925cddbb9e
emesc <tahder77@gmail.com>
2016-12-01 10:47:22 +1100

    Added page title helper for view files.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # def full_title(page_title="")
+  #   base_title = "Co Corp Inc"
+  #   sub_title = "Kickstarter prism direct trade"
+  #   page_title.empty? ? base_title + ' | ' + sub_title : base_title + ' | ' + page_title
+  # end
+  def title(page_title="")
+    content_for(:title) { page_title }
+  end
 end
