e357d1379b87001645ce0e3bef81043d3000624d
emesc <tahder77@gmail.com>
2016-12-02 11:52:16 +1100

    Used named routes on sections
    Added update and delete actions to subject.

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,20 +1,7 @@
 class SubjectsController < ApplicationController
 
   layout :user_layout
-
-  def show
-    # find all sections scoped through subject model
-    @subject = Subject.find(params[:id])
-    # long solution
-    # @sections = Section.where("subject_id = ? AND complete = ?", @subject.id, false)
-    # short solution, dynamic find through association
-    # @sections = @subject.sections.where(complete: false)
-    # update above line since this find is moved to the model, with sorting
-    # @sections = @subject.sections.incomplete
-
-    # render json: @sections
-    @sections = @subject.sections
-  end
+  before_action :find_subject, except: [:index]
 
   def index
     # to check if all subjects are valid
@@ -35,7 +22,48 @@ class SubjectsController < ApplicationController
     # render layout: false
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @subject.update_attributes(subject_params)
+      flash[:notice] = "Subject successfully updated."
+      redirect_to subjects_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    # find all sections scoped through subject model
+    # @subject = Subject.find(params[:id])
+    # long solution
+    # @sections = Section.where("subject_id = ? AND complete = ?", @subject.id, false)
+    # short solution, dynamic find through association
+    # @sections = @subject.sections.where(complete: false)
+    # update above line since this find is moved to the model, with sorting
+    # @sections = @subject.sections.incomplete
+
+    # render json: @sections
+    @sections = @subject.sections
+  end
+
+  def destroy
+    @subject.destroy
+    flash[:notice] = "Subject deleted successfully."
+    redirect_to subjects_path
+  end
+
   private
+
+    def find_subject
+      @subject = Subject.find(params[:id])
+    end
+
+    def subject_params
+      params.require(:subject).permit(:title)
+    end
 
     # control layouts based on user
     def user_layout
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'comments/create'
 
   authenticated :user do
-    root to: "products#index", as: :authenticated_root
+    # root "products#index", as: :authenticated_root
   end
-  root to: "welcome#index"
+  # root "welcome#index"
+  root "subjects#index"
   devise_for :users
   scope "/admin" do
     resources :users
@@ -12,13 +13,16 @@ Rails.application.routes.draw do
   # get "subjects",       to: "subjects#index"
   # get "subjects(/:id)", to: "subjects#show"
   # get "sections",       to: "sections#index"
-  resources :subjects, only: [:index, :show]
+  resources :subjects, except: [:new, :create]
   resources :sections, except: [:destroy] do
     resources :comments, only: [:create]
   end
   # resources :users,     only: [:index, :show, :new, :create, :update]
   resources :products
   resources :roles
-
+  # page not found
   get "*path", to: redirect("404.html")
+
+  # named route
+  get "/sections/:year/:month", to: "sections#archive", as: :section_archive
 end
